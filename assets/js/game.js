15f5310e7934de91af22bc1ef8ab0a70d8a5676b
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2021-11-08 22:22:56 -0500

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// this creates a function named "fight"
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+  }
+
+fight();
